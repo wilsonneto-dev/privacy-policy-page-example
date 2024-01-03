74fa8f71db1efb9500e7e8f9db77995c376be536
Wilson Neto B.R. <contato@wilsonneto.com.br>
2024-01-03 18:22:48 +0000

    quotes mark adjustments

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -4,30 +4,30 @@ export default function Home() {
         <h1>Byte Byte Dark (Dark Theme): User Data Privacy Policy</h1>
 
         <h2>Overview</h2>
-        <p>This Privacy Policy outlines our commitment to the privacy and protection of user data while using the "Byte Byte Dark (Dark Theme)" Chrome extension. Our extension is designed to enhance your browsing experience on Byte-Byte-Go's website by switching to a dark theme. We prioritize user privacy and do not collect, store, or transmit any personal or sensitive user data.</p>
+        <p>This Privacy Policy outlines our commitment to the privacy and protection of user data while using the &quote;Byte Byte Dark (Dark Theme)&quote; Chrome extension. Our extension is designed to enhance your browsing experience on Byte-Byte-Go&quote;s website by switching to a dark theme. We prioritize user privacy and do not collect, store, or transmit any personal or sensitive user data.</p>
 
         <h2>Data Collection and Use</h2>
-        <p><strong>No Data Collection</strong>: The "Byte Byte Dark (Dark Theme)" does not collect, store, or transmit any personal, sensitive, or browsing data. The extension operates entirely locally on your device.</p>
+        <p><strong>No Data Collection</strong>: The &quote;Byte Byte Dark (Dark Theme)&quote; does not collect, store, or transmit any personal, sensitive, or browsing data. The extension operates entirely locally on your device.</p>
         <p><strong>Browser Interaction</strong>: The extension uses browser navigation information solely to apply the dark theme style to the Byte-Byte-Go site. This interaction does not involve any data collection or storage.</p>
-        <p><strong>Consent and Control</strong>: As there is no data collection, no user consent is required for data handling. Users maintain full control over the extension's operation through their browser settings.</p>
+        <p><strong>Consent and Control</strong>: As there is no data collection, no user consent is required for data handling. Users maintain full control over the extension&quote;s operation through their browser settings.</p>
 
         <h2>Secure Data Handling (Not Applicable)</h2>
         <p><strong>Secure Transmission</strong>: Although our extension does not handle user data, we adhere to best practices for data security. The extension does not transmit any data, securely or otherwise.</p>
         <p><strong>No Unnecessary Data Handling</strong>: The extension does not collect, transmit, or disclose any user data, sensitive or otherwise, aligning with our policy of minimal and secure data handling.</p>
 
         <h2>Privacy Policy Compliance</h2>
-        <p><strong>Compliance with Chrome Web Store Policies</strong>: This Privacy Policy is in full compliance with the Chrome Web Store's developer program policies regarding personal or sensitive user data.</p>
+        <p><strong>Compliance with Chrome Web Store Policies</strong>: This Privacy Policy is in full compliance with the Chrome Web Store&quote;s developer program policies regarding personal or sensitive user data.</p>
         <p><strong>Transparency and User Control</strong>: We believe in transparency and user control. This policy clearly outlines our practices (or the lack thereof) concerning data collection and handling.</p>
         <p><strong>Commitment to Privacy</strong>: Our commitment to user privacy is paramount. The extension has been designed to respect user privacy completely and does not involve any form of user data processing.</p>
 
         <h2>User Rights and Choices</h2>
         <p><strong>User Control</strong>: Users have complete control over the extension and can disable or remove it at any time through their Chrome browser settings.</p>
-        <p><strong>No Offline Mode Required</strong>: Since our extension does not collect data, an "offline mode" is not applicable. The extension functions without any data collection, both online and offline.</p>
+        <p><strong>No Offline Mode Required</strong>: Since our extension does not collect data, an &quote;offline mode&quote; is not applicable. The extension functions without any data collection, both online and offline.</p>
 
         <h2>Modifications to the Policy</h2>
         <p><strong>Policy Updates</strong>: Any future updates to this policy will be consistent with our commitment to user privacy and will be communicated appropriately to our users.</p>
 
-        <p>We are dedicated to ensuring your digital privacy and security. If you have any questions regarding this policy or the "Byte Byte Dark (Dark Theme)" extension, please feel free to contact us.</p>
+        <p>We are dedicated to ensuring your digital privacy and security. If you have any questions regarding this policy or the &quote;Byte Byte Dark (Dark Theme)&quote; extension, please feel free to contact us.</p>
 
       </main>
   )
