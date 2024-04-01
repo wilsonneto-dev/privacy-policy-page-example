25f95aeccf4fe10bc408829c255f6c4513f264c7
Wilson Neto <contato@wilsonneto.com.br>
2024-03-31 23:32:41 -0300

    fix: scape quotes and apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ export default function Home() {
           <h1>Easy Snippets App: User Data Privacy Policy</h1>
 
           <h2>Overview</h2>
-          <p>This Privacy Policy reflects our commitment to the privacy and protection of user data within the "Easy
-                Snippets" app. "Easy Snippets" is an application designed to improve your productivity by allowing you
+          <p>This Privacy Policy reflects our commitment to the privacy and protection of user data within the &quot;Easy
+                Snippets&quot; app. &quot;Easy Snippets&quot; is an application designed to improve your productivity by allowing you
                 to save and organize code snippets directly on your device. We prioritize user privacy above all,
                 ensuring that all user data is stored locally on your computer or browser without any data collection,
                 storage, or transmission to external servers.</p>
 
           <h2>Data Collection and Use</h2>
-          <p><strong>Local Storage Only</strong>: "Easy Snippets" does not collect, store, or transmit any personal,
-                sensitive, or browsing data to external servers. All data is stored locally on the user's device,
+          <p><strong>Local Storage Only</strong>: &quot;Easy Snippets&quot; does not collect, store, or transmit any personal,
+                sensitive, or browsing data to external servers. All data is stored locally on the user&apos;s device,
                 ensuring complete privacy and control over their information.</p>
           <p><strong>Application Functionality</strong>: The app leverages local storage to save user preferences and
                 code snippets. This process is entirely local, with no external data interactions or dependencies.</p>
@@ -21,11 +21,11 @@ export default function Home() {
                 the ability to modify or delete their stored snippets at any time.</p>
 
           <h2>Secure Data Handling</h2>
-          <p><strong>Local Data Security</strong>: While "Easy Snippets" primarily operates with local data storage, we
-                advocate for and encourage users to maintain their device's security to protect their locally stored
+          <p><strong>Local Data Security</strong>: While &quot;Easy Snippets&quot; primarily operates with local data storage, we
+                advocate for and encourage users to maintain their device&apos;s security to protect their locally stored
                 data.</p>
           <p><strong>Privacy by Design</strong>: Our app is designed with privacy as a core principle, ensuring that
-                user data remains on the user's device without any external access or transmission.</p>
+                user data remains on the user&apos;s device without any external access or transmission.</p>
 
           <h2>Privacy Policy Compliance</h2>
           <p><strong>Compliance with App Store Policies</strong>: This Privacy Policy adheres to the policies of the
@@ -33,13 +33,13 @@ export default function Home() {
                 privacy.</p>
           <p><strong>Transparency and User Control</strong>: We are transparent about our data handling practices—since
                 we do not collect data, our policy centers on user control and local data management.</p>
-          <p><strong>Commitment to Privacy</strong>: Our dedication to user privacy is unwavering. "Easy Snippets" has
+          <p><strong>Commitment to Privacy</strong>: Our dedication to user privacy is unwavering. &quot;Easy Snippets&quot; has
                 been crafted to respect user privacy fully, with a strict local storage policy for all user data.</p>
 
           <h2>User Rights and Choices</h2>
-          <p><strong>User Control and Data Management</strong>: Users have absolute control over their data within "Easy
-                Snippets," including the ability to edit, delete, or backup their snippets as needed.</p>
-          <p><strong>Offline Functionality</strong>: Given the app's reliance on local storage, "Easy Snippets" is
+          <p><strong>User Control and Data Management</strong>: Users have absolute control over their data within &quot;Easy
+                Snippets,&quot; including the ability to edit, delete, or backup their snippets as needed.</p>
+          <p><strong>Offline Functionality</strong>: Given the app&apos;s reliance on local storage, &quot;Easy Snippets&quot; is
                 inherently functional both online and offline, without any compromise to privacy or functionality.</p>
 
           <h2>Modifications to the Policy</h2>
@@ -47,7 +47,7 @@ export default function Home() {
                 to this policy, ensuring they are communicated clearly to our users.</p>
 
           <p>We are dedicated to safeguarding your digital privacy and security. If you have any questions regarding
-                this policy or the "Easy Snippets" app, please do not hesitate to contact us.</p>
+                this policy or the &quot;Easy Snippets&quot; app, please do not hesitate to contact us.</p>
 
     </main>
   )
